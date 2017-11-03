5b2be4e06fc9c9b420a6b3af4be22810b851a098
brandongatlin <brandongatlin1981@me.com>
2017-11-02 20:10:09 -0500

    redid

--- fb_test.js
+++ fb_test.js
@@ -1,22 +1,17 @@
-FB.getLoginStatus(function(response) {
-    statusChangeCallback(response);
-});
-
-//The response object that's provided to your callback contains a number of fields:
-
 
 {
     status: 'connected',
     authResponse: {
         accessToken: '...',
-        expiresIn: '...',
-        signedRequest: '...',
-        userID: '...'
+        expiresIn:'...',
+        signedRequest:'...',
+        userID:'...'
     }
 }
 
+
 function checkLoginState() {
-            FB.getLoginStatus(function(response) {
-                statusChangeCallback(response);
-            });
-        }
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
+}
