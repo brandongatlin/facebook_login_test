0f1a59f0dbd6e74ff35eb6fa3ccfa74f37b56e0b
brandongatlin <brandongatlin1981@me.com>
2017-11-07 18:50:07 -0600

    added firebas to auth page

--- firebase_chat_fbauth_test1.js
+++ firebase_chat_fbauth_test1.js
@@ -3,5 +3,22 @@ var b = 2
 
 console.log(a+b); //3 = true
 
+console.log("firebase_chat_app.js loaded");
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCnD00DCXAEUzyEJQVNwA7yI7G5OUstYHs",
+    authDomain: "chat-test-56e7c.firebaseapp.com",
+    databaseURL: "https://chat-test-56e7c.firebaseio.com",
+    projectId: "chat-test-56e7c",
+    storageBucket: "chat-test-56e7c.appspot.com",
+    messagingSenderId: "1003382859041"
+  };
+  firebase.initializeApp(config);
+
+var database = firebase.database();
+var loginData = database.ref("/logins");
+
+var provider = new firebase.auth.FacebookAuthProvider();
 
 
